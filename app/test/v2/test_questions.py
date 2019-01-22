a17e3a3fc249d22bd9fd299a69e24a67c88ecea7
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-22 20:06:57 +0300

    [ch #163304765] update questions test

--- app/test/v2/test_questions.py
+++ app/test/v2/test_questions.py
@@ -1,43 +1,61 @@
+"""testing questions url api endpoints """
+import json
 import unittest
-import os
-import pytest
+# local imports
 from app import create_app
 
-class UsersTest(unittest.TestCase):
+class QuestionsTest(unittest.TestCase):
+    """Testing Question URL API endpoints"""
     def setUp(self):
         self.application = create_app
         self.app = self.application.test_client()
+        
+        self.question1 = {
+            "question_body":"Youtube ads are everything??",
+            "question_title":"Youtube"
+        }
 
-    """test api endpoints"""
-    def test_1_question_post(self):
-        """test user post question successfully (201)"""
-        pass
-    
-    def test_2_get_questions(self):
-        """test question posted by user already exist (409)"""
-        pass
-    
-    def test_3_get_specific_question(self):
-        """user should be able to get a specific questions so as to upvote or downvote (200)"""
-        pass
+        self.question2 = {
+            "question_body":"Youtube ads are everything",
+            "question_title":""
+        }
 
-    def test_4_user_upvote_question(self):
-        """test user upvote a question succesfully (204)"""
-        pass
+        self.question3 = {
+            "question_body":"",
+            "question_title":"Youtube"
+        }
+
+    def test_qood_question_post(self):
+        """test that user post a question with all required fields"""
+        response = self.app.post('/api/v2/meetup/1/question', data=json.dumps(self.question1), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_questions(self):
+        """test that user post a question with all required fields"""
+        response = self.app.get('/api/v2/meetup/1/question', data=json.dumps(self.question1), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_single_questions(self):
+        """test that user post a question with all required fields"""
+        response = self.app.get('/api/v2/meetup/1/question/1', data=json.dumps(self.question1), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
     
-    def test_5_user_downvote(self):
-        """test user downvotes a question succesfully (204)"""
-        pass
+    def test_upvote_questions(self):
+        """test that user post a question with all required fields"""
+        response = self.app.patch('/api/v2/meetup/1/question/1/upvote', data=json.dumps(self.question1), content_type='application/json')
+        self.assertEqual(response.status_code, 204)
     
-    """test validations"""
-    def test_question_exist(self):
-        """test that question exist when user inputs it twice (409)"""
-        pass
-    
-    def test_id_not_found(self):
-        """check when question id is missing (404)"""
-        pass
-    
-   
-    
-    
+    def test_downvote_questions(self):
+        """test that user post a question with all required fields"""
+        response = self.app.patch('/api/v2/meetup/1/question/1/downvote', data=json.dumps(self.question1), content_type='application/json')
+        self.assertEqual(response.status_code, 204)
+
+    def test_empty_title_question_post(self):
+        """test user input question with empty title"""
+        response = self.app.post('/api/v2/meetup/1/question', data=json.dumps(self.question2), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_empty_body_question_post(self):
+        """test user inputs"""
+        response = self.app.post('/api/v2/meetup/1/question', data=json.dumps(self.question3), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
