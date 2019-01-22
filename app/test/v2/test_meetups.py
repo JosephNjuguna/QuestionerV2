e21a9b46082a19cb0c7fba4ad9931c3b8ed68a8e
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-22 20:06:05 +0300

    [ch #163304765] update meetup test

--- app/test/v2/test_meetups.py
+++ app/test/v2/test_meetups.py
@@ -1,37 +1,47 @@
+"""test for api endpoints"""
+# inbuilt modelues
+import json
 import unittest
-import os
-import pytest
+# local imports
 from app import create_app
-
-class UsersTest(unittest.TestCase):
+class MeetupTest(unittest.TestCase):
     def setUp(self):
-        self.application = create_app
-        self.app = self.application.test_client()
-
+        self.appliaction = create_app
+        self.app = self.appliaction.test_client()
+        self.record1 = {
+            "location":"Kenya ,Safaricom",
+            "topic":"Da",
+            "happeningon":"1/1/2018",
+            "tag":"@comeall"
+        }
     """test api endpoints"""
-    def test_1_admin_create_meetup(self):
+    def test_admin_create_meetup(self):
         """test that admin post question successfully (201)"""
-        pass
+        response = self.app.post('/api/v2/meetup', data=json.dumps(self.record1), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
     
-    def test_2_get_upcoming_meetup(self):
-        """test that a meetup exist already (409 conflict error)"""
-        pass
-    def test_3_get_specific_meetup(self):
+    def test_get_all_meetup(self):
         """user should be able to get a specific meetup"""
-        pass
-
-    """test validations"""
-    def test_meetup_id_exist(self):
-        """test user tries to enter same meetup (409 conflict)"""
-        pass
+        response = self.app.get('/api/v2/meetup', data=json.dumps(self.record1), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
     
-    def test_fetch_nonexistance_id(self):
-        """test when user tries to fetch unavailable meetup id"""
-        pass
+    def test_get_upcoming_meetup(self):
+        """user should be able to get a specific meetup"""
+        response = self.app.get('/api/v2/meetup/upcoming', data=json.dumps(self.record1), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
-    def test_invalid_key_entry(self):
-        """test that input data there is an empty key (400 empty key)"""
-        pass
-
-    def test_empty_value_entry(self):
-        """test if admin tries to insert a dictkey which is empty (400)"""
+    def test_get_specific_meetup(self):
+        """user should be able to get a specific meetup"""
+        response = self.app.get('/api/v2/meetup/1', data=json.dumps(self.record1), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_delete_a_meetup(self):
+        """user should be able to get a specific meetup"""
+        response = self.app.delete('/api/v2/meetup/1', data=json.dumps(self.record1), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_update_a_meetup(self):
+        """user should be able to get a specific meetup"""
+        response = self.app.put('/api/v2/meetup/1', data=json.dumps(self.record1), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+    
