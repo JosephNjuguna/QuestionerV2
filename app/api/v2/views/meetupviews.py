1461a1aba77d9691a1f013bf2141ce8efd7ffcb7
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-23 05:14:12 +0300

    [starts #163303067] add create meetup api endpoint

--- app/api/v2/views/meetupviews.py
+++ app/api/v2/views/meetupviews.py
@@ -1,9 +1,47 @@
+import datetime
+#downloaded dependencies
 from flask_restful import Resource
+from flask import request, make_response, jsonify
+#local imports
+from app.api.v2.models.meetup import MeetUp
+from app.api.v2.utilis.validations import CheckData
+args = ""
 class PostMeetup(Resource):
     """admin create meetup endpoint"""
-    def __init__(self):
-        pass
-        
+    #@tokenrequired
+    def post(self):
+        try:
+            data = request.get_json()
+            createdon = datetime.datetime.utcnow()
+            location = data['location']
+            topic = data['topic']
+            happeningon = data['happeningon']
+            tags = data['tag']
+
+            """validate that no key is empty"""
+            CheckData.checkkey(data)
+
+            meetupdata= {
+                "createdon":createdon,
+                "location":location,
+                "topic": topic,
+                "happeningon": happeningon,
+                "tags":tags
+            }
+            """pass meetup dict to Meetup Models"""
+            meetup_db = MeetUp(**meetupdata)
+            saved = meetup_db.create_meetup()
+            meetup_id = saved
+            resp = {
+                "message": "Meetup Succesfully Created",
+                "meetup": topic,
+                "user_id": "{}".format(meetup_id)
+            }
+            meetup_db.close_db()
+            return resp,201
+        except KeyError:
+            return make_response(jsonify({"message":"Key Error"}),500)
+            
 class GetMeetup(Resource):
     def __init__(self):
         pass
