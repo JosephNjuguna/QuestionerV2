c9c1dc636acb172d3e98a9918bb409b0c1ceebd5
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-23 22:56:37 +0300

    [fti #163303083] add test for post question

--- app/test/v2/test_questions.py
+++ app/test/v2/test_questions.py
@@ -14,39 +14,36 @@ class QuestionsTest(unittest.TestCase):
             "question_body":"Youtube ads are everything??",
             "question_title":"Youtube"
         }
-
         self.question2 = {
-            "question_body":"Youtube ads are everything",
-            "question_title":""
+            "question_title":"",
+            "question_body":"Youtube ads are everything"
         }
-
         self.question3 = {
-            "question_body":"",
-            "question_title":"Youtube"
+            "question_title":"Youtube",
+            "question_body":""
         }
-
-    def test_qood_question_post(self):
+    def test_1_question_post(self):
         """test that user post a question with all required fields"""
         response = self.app.post('/api/v2/meetup/1/question', data=json.dumps(self.question1), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
-    def test_get_questions(self):
+    def test_2_questions(self):
         """test that user post a question with all required fields"""
         response = self.app.get('/api/v2/meetup/1/question', data=json.dumps(self.question1), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
 
-    def test_get_single_questions(self):
+    def test_3_single_questions(self):
         """test that user post a question with all required fields"""
         response = self.app.get('/api/v2/meetup/1/question/1', data=json.dumps(self.question1), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
     
-    def test_upvote_questions(self):
-        """test that user post a question with all required fields"""
+    def test_4_questions(self):
+        """test that user upvote question"""
         response = self.app.patch('/api/v2/meetup/1/question/1/upvote', data=json.dumps(self.question1), content_type='application/json')
         self.assertEqual(response.status_code, 204)
     
-    def test_downvote_questions(self):
-        """test that user post a question with all required fields"""
+    def test_5_questions(self):
+        """test that user down a question"""
         response = self.app.patch('/api/v2/meetup/1/question/1/downvote', data=json.dumps(self.question1), content_type='application/json')
         self.assertEqual(response.status_code, 204)
 
@@ -54,8 +51,8 @@ class QuestionsTest(unittest.TestCase):
         """test user input question with empty title"""
         response = self.app.post('/api/v2/meetup/1/question', data=json.dumps(self.question2), content_type='application/json')
         self.assertEqual(response.status_code, 400)
-
+    
     def test_empty_body_question_post(self):
-        """test user inputs"""
+        """test user input question with empty body"""
         response = self.app.post('/api/v2/meetup/1/question', data=json.dumps(self.question3), content_type='application/json')
         self.assertEqual(response.status_code, 400)
