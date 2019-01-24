3323af12347442a6405144111532f4e710328a3a
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-24 13:26:51 +0300

    [starts #163303075]start refactor code

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
+import os
+import psycopg2
 from flask import Flask
-
-# local import
+from instance.config import app_config
 from app.api import version_two as questioner_two
+from app.api.v2.models.basemodel import DatabaseConnection
 
-create_app = Flask(__name__)
+def create_app(name_conf):
+    my_app = Flask(__name__, instance_relative_config=True)
+    my_app.config.from_object(app_config[name_conf])
+    my_app.config.from_pyfile('config.py')
 
-create_app.register_blueprint(questioner_two, url_prefix="/api/v2")
+    db_url = app_config[name_conf].Database_Url
+
+    DatabaseConnection(db_url)
+    if name_conf == "testing":
+        DatabaseConnection.drop_all_tables(DatabaseConnection)
+    DatabaseConnection.create_tables_and_admin(DatabaseConnection)
+
+    my_app.register_blueprint(questioner_two, url_prefix="/api/v2")
+ 
+    return my_app
