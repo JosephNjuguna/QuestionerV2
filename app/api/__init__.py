f50a81fb8e7397e0dec2c1b63a6c7b3ee810755e
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-24 06:09:11 +0300

    [ft #163303079]add user rsvp route

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,8 +2,9 @@ from flask_restful import Api
 from flask import Blueprint
 #local imports
 
-from app.api.v2.views.userviews import (SignUp, LogIn, UserResetPassword, OneUser, UserUpdateInfo)
-from app.api.v2.views.meetupviews import (PostMeetup, GetMeetup ,UpcomingMeetup,SpecificMeetup,DeleteMeetUp,UpdateMeetUp)
+from app.api.v2.views.userviews import (SignUp, LogIn, OneUser)
+from app.api.v2.views.rsvpviews import (rsvp_meetup)
+from app.api.v2.views.meetupviews import (Meetup, SpecificUpcomingMeetup)
 from app.api.v2.views.questionsviews import (PostQuestion,GetQuestionsMeetup, GetSingleQuestion, UpvoteQuestion, DownVoteQuestion)
 version_two = Blueprint('version_two',__name__)
 api = Api(version_two)
@@ -11,17 +12,11 @@ api = Api(version_two)
 api.add_resource(SignUp, '/auth/signup')
 api.add_resource(LogIn,'/auth/login')
 api.add_resource(OneUser, '/users/<int:user_id>')
-#-------------------------------------------------------------
-api.add_resource(UserUpdateInfo, '/users/<int:user_id>/update')
-api.add_resource(UserResetPassword, '/auth/passwordreset')
 #meetup
-api.add_resource(PostMeetup, '/meetup')
-api.add_resource(GetMeetup, '/meetup')
-api.add_resource(UpcomingMeetup, '/meetup/upcoming')
-api.add_resource(SpecificMeetup, '/meetup/upcoming/<int:m_id>')
-#--------------------------------------------------------
-api.add_resource(DeleteMeetUp, '/meetup/<int:m_id>/delete')
-api.add_resource(UpdateMeetUp, '/meetup/<int:m_id>/update')
+api.add_resource(Meetup, '/meetup', '/meetup/upcoming', '/meetup/upcoming/<int:m_id>/delete')
+api.add_resource(SpecificUpcomingMeetup, '/meetup/upcoming/<int:m_id>')
+#rsvp meetup
+api.add_resource(rsvp_meetup, '/meetup/upcoming/<int:m_id>/rsvp')
 #questions
 api.add_resource(PostQuestion , '/meetup/<int:m_id>/question')
 api.add_resource(GetQuestionsMeetup , '/meetup/<int:m_id>/question')
