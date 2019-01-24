7befe40887a3124c1ae978e42a8a492de876c029
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-24 06:51:46 +0300

    [ft #163303079]rsvp view validations

--- app/api/v2/views/rsvpviews.py
+++ app/api/v2/views/rsvpviews.py
@@ -6,6 +6,7 @@ from flask import request, make_response, jsonify
 from app.api.v2.models.rsvp_models import rsvp_model
 from app.api.v2.utilis.validations import CheckData
 
+
 class rsvp_meetup(Resource):
     def post(self, m_id):
         try:
@@ -23,8 +24,8 @@ class rsvp_meetup(Resource):
             rsvp_data = {
                 "topic": topic,
                 "status": status,
-                "username":username,
-                "m_id":m_id
+                "username": username,
+                "m_id": m_id
             }
             rsvp_db = rsvp_model(**rsvp_data)
             rsvp_saved = rsvp_db.rsvp_meetup()
@@ -32,26 +33,26 @@ class rsvp_meetup(Resource):
             check_meetup = rsvp_db.check_meetup_exist(m_id, topic)
             check_user = rsvp_db.check_user_name(username)
             check_user_rsvpd = rsvp_db.check_user_name(username)
-            print(check_meetup)
-            print(check_user)
-            print(check_user_rsvpd)
-            response= {
+            response = {
                 "status": 201,
-                "message": "Successfully rsvp for the %s meetup" %(topic),
+                "message": "Successfully rsvp for the %s meetup" % (topic),
                 "user data": {
-                    "rsvp_id" :"{}".format(rsvp_saved),
+                    "rsvp_id": "{}".format(rsvp_saved),
                     "data": rsvp_data
                 }
             }, 201
-            if check_meetup != True :
-                return make_response(jsonify({"message":"Meetup id and topic not found"}),404)
-            if check_user != True:
-                return make_response(jsonify({"message":"Username not found"}),404)
-            if check_user_rsvpd == True:
-                return response
-            return make_response(jsonify({"message":"You have RSVP for this Meetup"}),409)
+            print(check_meetup)
+            print(check_user)
+            print(check_user_rsvpd)
+
+            if check_meetup == True:
+                if check_user == True:
+                    if rsvp_saved == True:
+                        return make_response(jsonify({"message": "You have RSVP for this Meetup"}), 409)
+                    return response
+                return make_response(jsonify({"message": "Username not found"}), 404)
+            return make_response(jsonify({"message": "Meetup id and topic not found"}), 404)
             rsvp_db.close_db()
 
         except KeyError:
             return make_response(jsonify({"status": 500, "error": "Expecting a topic, status, username field"}), 500)
-
