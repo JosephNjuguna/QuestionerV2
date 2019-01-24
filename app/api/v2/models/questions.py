7e2e94a3fc5bb15709c59c7ef6ff9bfa0223d592
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-24 13:28:22 +0300

    [ft #163303075]refactor code

--- app/api/v2/models/questions.py
+++ app/api/v2/models/questions.py
@@ -5,15 +5,13 @@ question database connection and  model
 import datetime
 #import uuid
 # local imports
-from app.api.v2.models.database import init_db
-from app.api.v2.models.basemodel import BaseModel
+from app.api.v2.models.basemodel import DatabaseConnection as connection
 #class
-class QuestionsModel(BaseModel):
+class QuestionsModel(connection):
     """questions class"""
     def __init__(self, question_body="question", question_title="title", meetup_id=0, user_id=0, votes=0):
         """class constructor"""
         #from BaseModel module
-        self.db = init_db()
         self.question_body = question_body
         self.question_title = question_title
         self.meetup_id = meetup_id
@@ -23,22 +21,30 @@ class QuestionsModel(BaseModel):
     
     def check_question_exist(self, question_body):
         """check if question already exists"""
-        curr = self.db.cursor()
         query = "SELECT  body FROM questions WHERE body = '%s'" % (question_body)
-        curr.execute(query)
-        return curr.fetchone() is not None
+        result = self.fetch_single_data_row(query)
+        return result
+
+    def check_question_id(self, q_id):
+        """check if question id exists"""
+        query = "SELECT id FROM questions WHERE id = '%s'" % (q_id)
+        result = self.fetch_single_data_row(query)
+        return result
 
     def post_question_db(self):
         """Add question details to the database"""
         if self.check_question_exist(self.question_body):
             return True
-        database = self.db
-        curr = database.cursor()
         query = """INSERT INTO questions (createdon, postedby, meetupid, title, body, votes)
             VALUES ('{}', '{}', '{}', '{}', '{}', '{}')RETURNING id;
             """.format(self.postedon, self.user_id, self.meetup_id, self.question_title, self.question_body, self.votes)
-        curr.execute(query)
-        question_id = curr.fetchone()[0]
-        database.commit()
-        curr.close()
-        return int(question_id)
+        result = self.save_incoming_data_or_updates(query)
+        return result
+    
+    #remaining
+    def upvote_question(self,m_id, q_id):
+        if self.check_question_exist(q_id):
+            return True
+        query = "SELECT id, createdon, postedby, meetupid, title, body, votes FROM questions WHERE id = '%s'" % (q_id)
+        data = self.fetch_single_data_row(query)
+        return data
